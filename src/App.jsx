83b5b4e0e3530c332f08accdd44f0b209d6c442e
mrvegetable2025-jpg <yourgithubemail@example.com>
2025-11-17 03:51:24 +0530

    UI update

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import LandingPage from "./pages/LandingPage";
 import OrdersPage from "./pages/OrdersPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import AdminDashboard from "./pages/AdminDashboard";
-
 import SuccessPage from "./pages/SuccessPage";
 import FloatingTools from "./components/FloatingTools";
 import AuthPage from "./pages/AuthPage";
 import AboutPage from "./pages/AboutPage";
 import ReviewsPage from "./pages/ReviewsPage";
 
+// ⭐ IMPORT SCROLL FIX
+import ScrollToTop from "./components/ScrollToTop";
+
 export default function App() {
   return (
     <>
       <Header />
+
+      {/* ⭐ AUTO SCROLL FIX */}
+      <ScrollToTop />
 
       <Routes>
         <Route path="/" element={<LandingPage />} />
@@ -25,13 +30,9 @@ export default function App() {
         <Route path="/success" element={<SuccessPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/reviews" element={<ReviewsPage />} />
-
         <Route path="/about" element={<AboutPage />} />
       </Routes>
 
-      
-
-      {/* ✅ Add this line */}
       <FloatingTools />
     </>
   );
